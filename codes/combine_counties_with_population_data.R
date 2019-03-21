b049385e6c27213df0833d5419eedeec5080941c
jms5151 <jamie.sziklay@gmail.com>
2019-03-21 10:12:38 -0700

    Update data to load.

--- codes/combine_counties_with_population_data.R
+++ codes/combine_counties_with_population_data.R
@@ -5,33 +5,14 @@ rm(list=ls()) #remove previous variable assignments
 library(plyr)
 
 # load data
-aedes.files <- list.files("data/")
-
-for (i in 1:length(aedes.files)){
-  filePath <- paste0("data/", aedes.files[i])
-  x <- read.csv(filePath, head=T, stringsAsFactors = F)
-  fileName <- substr(aedes.files[i], 1, nchar(aedes.files[i])-4)
-  assign(fileName, x)
-}
-
-# rbind aedes data
-aedes_collections <- do.call(rbind, list(aedes_collections_california
-                                       , aedes_collections_connecticut
-                                       , aedes_collections_florida
-                                       , aedes_collections_new_jersey
-                                       , aedes_collections_new_york
-                                       , aedes_collections_north_carolina
-                                       , aedes_collections_texas
-                                       , aedes_collections_wisconsin))
-
-aedes_collections$Date <- paste(aedes_collections$year, aedes_collections$month, "01", sep='-')
-aedes_collections$Date <- as.Date(aedes_collections$Date, "%Y-%m-%d")
+load("concatenated_data/aedes_collections.RData")
+AC_county_coords <- read.csv("data/AC_county_coords.csv", head=T, stringsAsFactors=F)
+us_census_data <- read.csv("data/us_census_data.csv", head=T, stringsAsFactors=F)
 
 # subset by unique counties and calculate min and max date by county
 aedes_collections <- ddply(aedes_collections, .(state, statefp, county, countyfp), summarize
                             , minimum_date = min(Date)-180
                             , maximum_date = max(Date))
-
 
 # format demographic data
 us_census_2015 <- us_census_data[,c("STATE", 'COUNTY', "STNAME", "CTYNAME", "POPESTIMATE2015", "RDEATH2015", "RBIRTH2015")]
